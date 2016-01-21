1f6811b06828119ff24cad5813f6fd4c556b9fb9
T0rd <agutorov@ua.fm>
2016-01-21 16:46:24 +0200

    Finish demo app

--- project/demo_app/config/initializers/session_store.rb
+++ project/demo_app/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
+DemoApp::Application.config.secret_key_base = 'your-secret'
 Rails.application.config.session_store :cookie_store, key: '_demo_app_session'
